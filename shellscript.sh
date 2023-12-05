804f4a7cf9b1b40d720cc295d9995bf358ddb7b1
maheshchanugondla <104671332+maheshchanugondla@users.noreply.github.com>
2023-12-05 12:29:27 +0530

    first commit

---
+++ shellscript.sh
@@ -0,0 +1,14 @@
+! /bin/bash 
+arr_csv=() 
+while IFS= read -r line 
+do
+    arr_csv+=("$line")
+done < input.csv
+
+echo "Displaying the contents of array mapped from csv file:"
+index=0
+for record in "${arr_csv[@]}"; do
+    disk_usage=$(df -h)
+    echo "Record at index-${index} : $record"
+    ((index++))
+	done
